0d567a14a56afccbc91c2b22cbdc4723f15bd606
xdkurbie <60493774+xdkurbie@users.noreply.github.com>
2026-04-16 21:33:26 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -8,3 +8,9 @@ module.exports = {
     idPrefixes: ["tt"],
     catalogs: []
 };
+{
+  "stremioAddonsConfig": {
+    "issuer": "https://stremio-addons.net",
+    "signature": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..MTiS_gV688qC76COJdnTww.1Xl9w46cgABUE3TQnSzcbjlgJ0KJF5aUa7zTixz1QA0bbqV3tT0aDSG9K658P0p7SC7QjAc0s8ACKAC6RKEAUy_F_kqk3VCqkWGgyDdhT1jIekEZCbl098ptMJzMa6a1.vnbn8RSlI5Knmk-KS3W8nQ"
+  }
+}
