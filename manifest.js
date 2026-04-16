f516f20f9f1a7281967a05707f8d14bea92749f7
xdkurbie <60493774+xdkurbie@users.noreply.github.com>
2026-04-16 21:38:50 +0200

    Remove stremioAddonsConfig from manifest.js
    Removed stremioAddonsConfig section from manifest.

--- manifest.js
+++ manifest.js
@@ -1,7 +1,4 @@
 module.exports = {
-  "stremioAddonsConfig": {
-    "issuer": "https://stremio-addons.net",
-    "signature": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..MTiS_gV688qC76COJdnTww.1Xl9w46cgABUE3TQnSzcbjlgJ0KJF5aUa7zTixz1QA0bbqV3tT0aDSG9K658P0p7SC7QjAc0s8ACKAC6RKEAUy_F_kqk3VCqkWGgyDdhT1jIekEZCbl098ptMJzMa6a1.vnbn8RSlI5Knmk-KS3W8nQ"
     id: "com.norge.hub",
     version: "1.0.0",
     name: "NORGE-HUB",
